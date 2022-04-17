73e06d601e5ef2b7c7a68b2b8b098f10fbe63254
NicolasZapataValdes <nico.v010@hotmail.com>
2022-04-16 19:48:07 -0500

    Si

---
+++ SGP/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGP.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult ActualizarInformacion()
+        {
+            return View();
+        }
+
+        public IActionResult PagoMatricula()
+        {
+            return View();
+        }
+    }
+}
