d3c22a7777b27180c1d3953f58d22961366beb31
Juan Ceballos <juandavi789@hotmail.com>
2022-04-25 00:03:35 -0500

    Modelo para el programa

---
+++ SGP/Models/Programa.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SGP.Models
+{
+    public class Programa
+    {
+        public int IdPrograma { get; set; }
+
+        public string ValPrograma { get; set; }
+
+        public string Pensum { get; set; }
+    }
+}
