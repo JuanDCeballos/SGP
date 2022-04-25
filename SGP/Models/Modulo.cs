61b9d4044ce6c7b3393a2117949f069df1d882e8
Juan Ceballos <juandavi789@hotmail.com>
2022-04-25 00:02:13 -0500

    Modelo para el modulo

---
+++ SGP/Models/Modulo.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SGP.Models
+{
+    public class Modulo
+    {
+        public int IdModulo { get; set; }
+
+        public string ValModulo { get; set; }
+
+        public int Creditos { get; set; }
+
+        public int IdPrograma { get; set; }
+    }
+}
