812cf13174a07b35bf6ceca5c46667c3fc79b08c
Juan Ceballos <juandavi789@hotmail.com>
2022-04-25 00:03:12 -0500

    Modelo para programa usuario

---
+++ SGP/Models/ProgramaUsuario.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SGP.Models
+{
+    public class ProgramaUsuario
+    {
+        public int IdProgramaUsuario { get; set; }
+
+        public int IdUsuario { get; set; }
+
+        public int IdPrograma { get; set; }
+    }
+}
